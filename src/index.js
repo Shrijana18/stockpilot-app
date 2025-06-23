c1e74b6b55bb3f5bf3c5ad1d3043bb0ba886649f
Shrijana18 <Shrijanakwade18@gmail.com>
2025-06-23 21:51:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Register from './Register';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Register />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
